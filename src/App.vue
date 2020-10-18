dfe1c639358b8a25b8ea6df617e0c1dffa34698f
juan251189 <juan2511@hotmail.com>
2020-10-18 19:55:32 +1100

    Instructor review

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <h1 v-myon:jumbotron="function jumbotron(){}">Directives Exercise</h1>
+      <button class="btn btn-primary" v-customOn:click="clicked">click me </button>
+      <hr>
+      <div style="width:150px;height:150px;background-color:lightgreen"
+        v-customOn:mouseenter="mouseEnter" v-customOn:mouseleave="mouseLeave"
+      ></div>
+      <!-- Exercise -->
+      <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+  directives: {
+    'customOn': {
+      bind(el, binding) {
+        //  el.onclick = function (){
+        //    binding.value();
+        //  }
+        const type = binding.arg;
+        const fn = binding.value;
+        el.addEventListener(type,fn);
+
+      }
+
+    }
+  },
+  methods:{
+    clicked(){
+      alert("hi there thanks for clicking ");
+    },
+    mouseEnter(){
+      console.log("mouse enter");
+    },
+    mouseLeave(){
+      console.log("mouse leave");
+    }
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('myon',{
+  bind(el,binding){
+    if(binding.arg == 'jumbotron'){
+      if(typeof binding.value === "function"){
+          el.style.height='300px';
+          el.style.margin='0 auto';
+          el.style.textAlign='center';
+          el.style.backgroundColor="#375288";
+      }
+
+    }
+  }
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
